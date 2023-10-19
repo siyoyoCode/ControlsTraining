17a2a539c9cd3112f9388cfed3e4cf6dcfdf83f4
siyoyoCode <siyona101215@gmail.com>
2023-10-19 16:42:50 -0700

    I did controls training

---
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+public class ExampleSubsystem extends SubsystemBase {
+
+  private final CANSparkMax motor;
+  private final CANSparkMax motor2;
+  private final XboxController controller;
+  private final JoystickButton button;
+
+  /** Creates a new ExampleSubsystem. */
+  public ExampleSubsystem() {
+    motor = new CANSparkMax(1, MotorType.kBrushless);
+    motor2 = new CANSparkMax(2, MotorType.kBrushless);  
+    controller = new XboxController(0);
+    button = new JoystickButton(controller, XboxController.Button.kA.value);
+    motor2.follow(motor);
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public CommandBase exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // motor.set(0.5);
+    motor2.set(controller.getLeftY());
+     //in periodic
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    
+    // This method will be called once per scheduler run during simulation
+  }
+}
