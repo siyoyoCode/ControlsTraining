d9eb08c9e253d4e5ee65003ca434973a4c7497b3
siyoyoCode <siyona101215@gmail.com>
2023-10-21 23:34:42 -0700

    finalized code for ball rolling robot

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -5,27 +5,55 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ColorSensorV3;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.motorcontrol.Talon; 
+
 
 public class ExampleSubsystem extends SubsystemBase {
 
-  private final CANSparkMax motor;
-  private final CANSparkMax motor2;
-  private final XboxController controller;
-  private final JoystickButton button;
+  //motors
+  private final WPI_TalonSRX motorTop; 
+  private final WPI_TalonSRX motorBottom;
+  private final CANSparkMax flyWheel;
+
+  //sensors
+  private final AnalogPotentiometer irBottom; //ir camera on the side
+  private final AnalogPotentiometer irTop; //ir camera on the side
+  private final ColorSensorV3 colorSensor; //color sensor
+  
+  //boleans
+  private boolean spaceAtTop; //chekcs if there is a ball at top
+  private boolean toLaunch; //launching sequence once controller button pressed
+
+  //controllers
+  private final XboxController controller; 
 
   /** Creates a new ExampleSubsystem. */
   public ExampleSubsystem() {
-    motor = new CANSparkMax(1, MotorType.kBrushless);
-    motor2 = new CANSparkMax(2, MotorType.kBrushless);  
+    //motors
+    motorTop = new WPI_TalonSRX(14);
+    motorBottom = new WPI_TalonSRX(13);  
+    flyWheel = new CANSparkMax(15, MotorType.kBrushed);
+    motorTop.configFactoryDefault();
+    motorBottom.configFactoryDefault();
+    
+    //sensors
+    irBottom = new AnalogPotentiometer(0); //IR sensor
+    irTop = new AnalogPotentiometer(1);
+    colorSensor = new ColorSensorV3(I2C.Port.kMXP); //color sensor
+
+    //controller and boolean
     controller = new XboxController(0);
-    button = new JoystickButton(controller, XboxController.Button.kA.value);
-    motor2.follow(motor);
+    spaceAtTop = true;
+    toLaunch = false;
   }
 
   /**
@@ -54,10 +82,48 @@ public class ExampleSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // motor.set(0.5);
-    motor2.set(controller.getLeftY());
-     //in periodic
-    // This method will be called once per scheduler run
+    System.out.println(irBottom.get());
+    //System.out.println("Hello");
+    double bottomBallTolerance = 0.08; //ideally 0.16
+    double topBallTolerance = 0.4; //further away means smaller
+    int middleBallTolerance = 9;  //add half of ball width, 1695-99(without)
+
+    //checks if there is space above for the balls to travel up
+    if(irTop.get() > topBallTolerance){
+      spaceAtTop = false;
+      motorBottom.set(0);
+      motorTop.set(0);
+      flyWheel.set(0);
+    }
+
+    //checks if there is a ball in 'waiting area' and if there is space for a ball to be rolled in
+    if(spaceAtTop && irBottom.get() >= bottomBallTolerance){
+        motorBottom.set(-0.2); //TEST SPEED
+
+        //checks if there is a ball that needs to move up before another can be rolled in
+        if(colorSensor.getProximity() > middleBallTolerance){
+          motorTop.set(-0.1);
+        }
+    } else { //resets 
+      motorBottom.set(0);
+      motorTop.set(0);
+    }
+
+    //This method will be called once per scheduler run
+
+    if(controller.getAButtonPressed()){
+      toLaunch = true;
+    }
+
+    if(toLaunch){
+      if(irTop.get() < topBallTolerance){
+        motorTop.set(-0.3);
+      } else {
+        flyWheel.set(0.3);
+        motorTop.set(0);
+        toLaunch = false;
+      }
+    }
   }
 
   @Override
